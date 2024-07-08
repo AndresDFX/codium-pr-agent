379c65422dd663fd6d96744ddb153b6c3e7a0812
Julian <andres_dfx@hotmail.com>
2024-07-08 18:00:46 -0500

    first commit

---
+++ example.py
@@ -0,0 +1,10 @@
+def add_numbers(a, b):
+    return a+b
+
+def greet(name):
+    print("Hello, " + name + "!")
+    return True
+
+if __name__ == "__main__":
+    print(add_numbers(1, 2))
+    greet("World")
